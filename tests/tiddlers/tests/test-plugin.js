7cde46fab2928f06aa5ce171857617232e76f2fa
bimlas <bimbalaszlo@gmail.com>
2024-02-09 19:18:13 +0100

    Simplify scripts and tests

---
+++ tests/tiddlers/tests/test-plugin.js
@@ -0,0 +1,21 @@
+/*\
+title: test-plugin.js
+type: application/javascript
+tags: [[$:/tags/test-spec]]
+
+Plugin tests
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+describe("Plugin tests", function() {
+	it("should work",function() {
+		expect(true).toBe(true);
+	});
+});
+
+})();
